048d4ba26ba98e3dbcd81679d394b4260d50c03b
Sandeep Kumar Sharma <sandeep_sharma@Sandeep-MacBook-Pro.local>
2018-02-05 16:00:12 +0530

    test 5 extention

--- EslintintegrationTesting/__tests__/App.js
+++ EslintintegrationTesting/__tests__/App.js
@@ -5,4 +5,4 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-it('renders correctly', () => const tree = renderer.create(<App />));
+it('renders correctly', () => renderer.create(<App />));
